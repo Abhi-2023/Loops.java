f3c2dab94ffd256b76db1307f8010c678eef086b
Abhishek Gupta <Abhishekgupta862004@gmail.com>
2023-01-29 23:06:15 +0530

    First Commit For Assignment

---
+++ Loops.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Loops {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of lines");
+        int n = sc.nextInt();
+            for ( int i = 0; i <n; i++){
+                System.out.print(i + " ");
+            }
+
+// int i = 1;
+//         do {
+//             int j = 1;
+//             do{
+//                 System.out.print("*");
+                
+//                 j++;
+                
+//             }while( j < i);
+//             System.out.println();
+//                 i++;
+//         }while (i < lines);
+        // for ( int i = 0; i < lines; i++){
+        //     for ( int j = 0; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        
+
+    }
+}
