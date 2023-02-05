53df7c22414169e16db5413d4db71d9ebf339a51
Abhishek Gupta <Abhishekgupta862004@gmail.com>
2023-02-05 14:36:06 +0530

    First commit in Assignments files

---
+++ AssignmentStrings.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class AssignmentStrings {
+ public static void main (String args[]){
+    // Question no 1 ..................
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your name");
+    String Name = sc.nextLine();
+    System.out.println("Entered name is " + Name + ", is it correct?");
+
+    // Question no 3..............
+
+    String solution1 = "My name is Abhishek";
+    System.out.println(solution1.length());
+
+    // Question no 5..........
+
+    String solution = "refrigerator";
+    System.out.println(solution.length());
+
+
+    // Question no 6...........
+
+    String solution2 = "Umbrella";
+    System.out.println(solution2.contains("U"));
+
+    
+    // Question no 7..............
+        // doesn't have informatio about it i searched it on youtube & on google but didn't understood it properly..................
+
+
+}   
+}
