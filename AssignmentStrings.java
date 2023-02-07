2512449b1034c8664dd1a91b791e590e67830018
Abhishek Gupta <Abhishekgupta862004@gmail.com>
2023-02-07 23:14:17 +0530

    second commit to this files as of 2 assignment

--- AssignmentStrings.java
+++ AssignmentStrings.java
@@ -2,32 +2,61 @@ import java.util.*;
 
 public class AssignmentStrings {
  public static void main (String args[]){
-    // Question no 1 ..................
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter your name");
-    String Name = sc.nextLine();
-    System.out.println("Entered name is " + Name + ", is it correct?");
+    // // Question no 1 ..................
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter your name");
+    // String Name = sc.nextLine();
+    // System.out.println("Entered name is " + Name + ", is it correct?");
 
-    // Question no 3..............
+    // // Question no 3..............
 
-    String solution1 = "My name is Abhishek";
-    System.out.println(solution1.length());
+    // String solution1 = "My name is Abhishek";
+    // System.out.println(solution1.length());
 
-    // Question no 5..........
+    // // Question no 5..........
 
-    String solution = "refrigerator";
-    System.out.println(solution.length());
+    // String solution = "refrigerator";
+    // System.out.println(solution.length());
 
 
-    // Question no 6...........
+    // // Question no 6...........
 
-    String solution2 = "Umbrella";
-    System.out.println(solution2.contains("U"));
+    // String solution2 = "Umbrella";
+    // System.out.println(solution2.contains("U"));
 
     
     // Question no 7..............
         // doesn't have informatio about it i searched it on youtube & on google but didn't understood it properly..................
 
+
+    // Day 17 Assingnment Solutions
+
+    // Question no..2.....
+
+    String input = "PWSKILLS";
+
+    String result = "";
+
+    for ( int i = input.length()-1; i >=0; i--){
+        result += input.charAt(i);
+    }
+    System.out.println(result);
+
+    // Question no...... 3.......
+
+    String input1 = "Think Twice";
+    String result1 = "";
+    String sarr[] = input1.split(" ");
+
+    for(String elem : sarr){
+        for ( int i = elem.length() - 1; i >= 0; i--){
+            result1 += elem.charAt(i);
+        }
+        result1 += " ";
+    }
+
+
+    System.out.println(result1);
 
 }   
 }
